7dcb5f4d6f53eb56b7bab2da36f88b04cb9e7bc5
DESKTOP-N850CB5\codes <codesonics@gmail.com>
2021-09-30 23:37:37 +0900

    Implement Sort Service 정렬 구현체를 사용하는 서비스를 별도로 작성 의존성 주입 방법을 적용

--- src/main/java/kr/codesonics/oop/Main.java
+++ src/main/java/kr/codesonics/oop/Main.java
@@ -1,13 +1,15 @@
 package kr.codesonics.oop;
 
 import kr.codesonics.oop.logic.BubbleSort;
+import kr.codesonics.oop.logic.JavaSort;
+import kr.codesonics.oop.logic.Sort;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<Integer> sort = new BubbleSort<>();
+        Sort<Integer> sort = new JavaSort<>();
         List arList = Arrays.asList(args);
         System.out.println("[Result] " + sort.sort(arList));
     }
